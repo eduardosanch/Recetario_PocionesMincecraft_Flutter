2478d770e9e33d9a60943ae0ff6b215a2353869a
eduardosanch <eduardosanch2006@gmail.com>
2026-05-08 08:30:17 -0600

    Documentacion

--- demo/tweeter/lib/core/api_config.dart
+++ demo/tweeter/lib/core/api_config.dart
@@ -2,7 +2,7 @@ class ApiConfig {
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
     defaultValue:
-        'https://recetario-pocionesminecraft-flutter.onrender.com',
+        'https://recetario-pocionesmincecraft-flutter.onrender.com',
   );
 
   static String get authUrl => '$baseUrl/api/auth';
